f58d308d5f69b9481ef40189ff5c502ea862ea79
Bassanelli Simone <bassanelli.simone.studente@itispaleocapa.it>
2023-11-21 12:34:57 +0100

    Get e set funzione base

---
+++ 10 - Classe articoli/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10___Classe_articoli
+{
+    internal class Articolo
+    {
+        protected int _codice;
+        protected string _name;
+        protected double _prezzo;
+        protected bool _carta = false;
+
+        static  int codice
+        {
+            get { return codice;}
+            set { codice = value;}
+        }
+        
+        static string name
+        {
+            get { return name;}
+            set { name = value;}
+        }
+        static double prezzo 
+        {
+            get { return prezzo;}
+            set { prezzo = value;}
+        }
+
+
+        public Articolo(int Codice, string Name, double Prezzo, bool Carta)
+        {
+            this._codice = Codice;
+            this._name = Name;
+            this._prezzo = Prezzo;
+            this._carta = Carta;
+        }
+
+        public double Sconto
+        {
+            get
+            {
+                if(_carta == true )
+                {
+                    return _prezzo = _prezzo - (_prezzo / 100) * 5;
+                }
+                else
+                {
+                    return _prezzo;
+                }
+            }
+        }
+    }
+}
--- 10 - Classe articoli/Form1.cs
+++ 10 - Classe articoli/Form1.cs
@@ -6,5 +6,10 @@ namespace _10___Classe_articoli
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
